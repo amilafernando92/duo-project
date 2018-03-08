204334a3200be578ce04238952d1de30f9b3adef
Amila Fernando <amila_fernandp92@gmail.com>
2018-03-08 16:51:54 +0100

    salvataggio

--- javascript/index.js
+++ javascript/index.js
@@ -232,7 +232,9 @@ var users = [
   ];
 
 $(document).ready(function () {
-    createBody ();
+	if ($("html").attr ("data-page")==="index") {
+		createBody ();
+	}
     addImgHeader ();
 });
 
@@ -247,37 +249,37 @@ function createBody () {
             //creazione del h1 nome e aggiunge all'ancora
             var h1name = document.createElement ("h1");
             h1name.setAttribute ("id", "h1name"+cont);
-            h1name.className = "col-md-4";
+            h1name.className = "col-md-4 col-sm-4";
             h1name.innerHTML = users[cont].name;
             a.appendChild (h1name);
             //creazione del h1 e-mail e aggiunge all'ancora
             var h1email = document.createElement ("h1");
             h1email.setAttribute ("id", "h1email"+cont);
-            h1email.className = "col-md-4";
+            h1email.className = "col-md-4 col-sm-4";
             h1email.innerHTML = users[cont].email;
             a.appendChild (h1email);
             //creazione del h1 - e aggiunge all'ancora
             var h1span = document.createElement ("h1");
             h1span.setAttribute ("id", "h1span"+cont);
-            h1span.className = "col-md-1";
+            h1span.className = "col-md-1 col-sm-1";
             h1span.innerHTML = "-";
             a.appendChild (h1span);
             //creazione del h1 numero di post e aggiunge all'ancora
             var h1npost = document.createElement ("h1");
             h1npost.setAttribute ("id", "h1npost"+cont);
-            h1npost.className = "col-md-1";
+            h1npost.className = "col-md-1 col-sm-1";
             h1npost.innerHTML = Math.ceil(Math.random()*10);
             a.appendChild (h1npost);
             //creazione del h1 post e aggiunge all'ancora
             var h1post = document.createElement ("h1");
             h1post.setAttribute ("id", "h1post"+cont);
-            h1post.className = "col-md-1";
+            h1post.className = "col-md-1 col-sm-1";
             h1post.innerHTML = "Post";
             a.appendChild (h1post);
             //creazione del h1 span per hover e aggiunge all'ancora
             var h1span = document.createElement ("h1");
             h1span.setAttribute ("id", "h1span"+cont);
-            h1span.className = "col-md-1";
+            h1span.className = "col-md-1 col-sm-1";
             var spanarrow = document.createElement ("span");
             spanarrow.setAttribute ("id", "spanarrow"+cont);
             spanarrow.className = "glyphicon glyphicon-arrow-right";
@@ -290,21 +292,18 @@ function createBody () {
 }
 
 function addImgHeader () {
-  if(document.getElementById ("imgheader")==null) {
-    //creazione del img con i suoi attributi
-    var imgheader = document.createElement ("img");
-    imgheader.setAttribute ("id", "imgheader");
-    imgheader.setAttribute ("src", "https://blacksaildivision.com/wp-content/uploads/2015/03/centos-users-and-groups-624x390.jpg");
-    imgheader.setAttribute ("srcset", "https://blacksaildivision.com/wp-content/uploads/2015/03/centos-users-and-groups.jpg 624w, https://blacksaildivision.com/wp-content/uploads/2015/03/centos-users-and-groups-300x188.jpg 300w");
-    imgheader.setAttribute ("sizes", "(max-width: 624px) 100vw, 624px");
-    //cerca div madre e aggiunge l'immagine ad esso
-    document.getElementById ("imgcontainer").appendChild (imgheader);
-  } else {
-    //da implementare
-    $("#imgheader").attr ("src", "");
-    $("#imgheader").attr ("srcset", "");
-    $("#imgheader").attr ("sizes", "");
-  }
+	//creazione del img con i suoi attributi
+	var imgheader = document.createElement ("img");
+	imgheader.setAttribute ("id", "imgheader");
+	imgheader.setAttribute ("src", "https://blacksaildivision.com/wp-content/uploads/2015/03/centos-users-and-groups-624x390.jpg");
+	imgheader.setAttribute ("srcset", "https://blacksaildivision.com/wp-content/uploads/2015/03/centos-users-and-groups.jpg 624w, https://blacksaildivision.com/wp-content/uploads/2015/03/centos-users-and-groups-300x188.jpg 300w");
+	imgheader.setAttribute ("sizes", "(max-width: 624px) 100vw, 624px");
+	//creazione dell'ancora con i suoi attributi
+	var a = document.createElement ("a");
+	a.setAttribute ("href", "index.html");
+	a.appendChild (imgheader);
+	//cerca div madre e aggiunge l'immagine ad esso
+	document.getElementById ("imgcontainer").appendChild (a);
 }
 
 function changeAll () {
